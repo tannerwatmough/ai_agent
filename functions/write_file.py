381ab91468d4a2279fb6b5a7410c4946ac3e6eaa
Tanner Watmough <79290989+tannerwatmough@users.noreply.github.com>
2025-09-28 18:03:51 -0600

    added the write_file function which will write or overwrite provided content (so AI agent can fix bugs in code)

---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+from functions.config import MAX_CHAR_LIMIT
+
+def write_file(working_directory, file_path, content):
+	try:
+		# join working directory and file_path
+		file_path = os.path.join(working_directory, file_path)
+
+		# Makes sure it is within the specified working_directory including path traversal
+		if not os.path.abspath(file_path).startswith(os.path.abspath(working_directory)):
+			return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+		
+		# write out content to file
+		with open(file_path, "w") as f:
+			f.write(content)
+		
+		return f'Successfully wrote to "{file_path} ({len(content)} characters written)'
+
+	except Exception as e:
+		return f"Error: {e}"
+
+
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def main():
   # TESTS FOR get_files_info
@@ -14,13 +15,20 @@ def main():
   # TESTS FOR get_file_content
   # print("Result for 'lorem.txt':\n" + get_file_content("calculator", "lorem.txt"))
 
-  print("Result for 'main.py':\n" + get_file_content("calculator", "main.py"))
+  # print("Result for 'main.py':\n" + get_file_content("calculator", "main.py"))
 
-  print("Result for 'pkg/calculator.py' directory:\n" + get_file_content("calculator", "pkg/calculator.py"))
+  # print("Result for 'pkg/calculator.py' directory:\n" + get_file_content("calculator", "pkg/calculator.py"))
 
-  print("Result for '/bin/cat' directory:\n" + get_file_content("calculator", "/bin/cat"))
+  # print("Result for '/bin/cat' directory:\n" + get_file_content("calculator", "/bin/cat"))
 
-  print("Result for 'pkg/does_not_exist.py' directory:\n" + get_file_content("calculator", "pkg/does_not_exist.py"))
+  # print("Result for 'pkg/does_not_exist.py' directory:\n" + get_file_content("calculator", "pkg/does_not_exist.py"))
+
+  # TESTS FOR write_file
+  print("Result for 'lorem.txt':\n" + write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+
+  print("Result for 'pkg/morelorem.txt':\n" + write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+
+  print("Result for '/tmp/temp.txt':\n" + write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 if __name__ == "__main__":
     main()
