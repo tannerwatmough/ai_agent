b663544d96ad0fca415c72b804fab8f1a62bcaa1
Tanner Watmough <79290989+tannerwatmough@users.noreply.github.com>
2025-09-28 19:17:55 -0600

    added ability to give system instructions through system prompts to control personality, rules, etc.

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
 
     print(response.text)
     if verbose_fl:
